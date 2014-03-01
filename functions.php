c0e7cbe697fc7a070c4c5ffe86a14d6eb047f9cc
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-01 13:57:54 -0800

    Base Media Library class

--- functions.php
+++ functions.php
@@ -1,3 +1,28 @@
 <?php
 
+class Media_Library {
 
+	private static $instance;
+
+	public static function get_instance() {
+
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new Media_Library;
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+		/** Do nothing **/
+	}
+
+}
+
+/**
+ * Load the theme
+ */
+function Media_Library() {
+	return Media_Library::get_instance();
+}
+add_action( 'after_setup_theme', 'Media_Library' );
