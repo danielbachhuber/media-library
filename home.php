385c8f11b1fabc17f369891bd440224e77128320
Daniel Bachhuber <d@danielbachhuber.com>
2014-03-01 15:07:16 -0800

    Break the attachment list up by date

--- home.php
+++ home.php
@@ -1,8 +1,26 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
+<?php if ( have_posts() ) :
+	$date_shown = false;
+	?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post();
+
+		if ( ! $date_shown || $date_shown != get_the_time( 'Y-m-d' ) ) : ?>
+
+			<h3><?php
+
+				$date_format = 'l, F j';
+				if ( get_the_time( 'Y' ) !== date( 'Y' ) ) {
+					$date_format .= ', Y';
+				}
+				the_time( $date_format );
+
+			?></h3>
+
+		<?php 
+			$date_shown = get_the_time( 'Y-m-d' );
+		endif; ?>
 
 		<?php get_template_part( 'parts/attachment/square-thumbnail' ); ?>
 
